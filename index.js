5203e58b7a540e6deccb8258a9044005e3cf3c65
bmanek <41875460+bmanek@users.noreply.github.com>
2019-05-10 15:16:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution here!
+// Write your solution here
+
+// Define 4 const called: append, prepend, removeLast and removeFirst, and set all to an initial value of ["Milo", "Otis", "Garfield"]. These are the arrays that we'll work with for each exercise.
+
+// To recap, push() pushes elements onto the ends of arrays, and pop() pops them off; similarly, unshift() adds elements to the beginnings of arrays, and shift() pulls them off. Now it's time to practice what we've learned.
+
+
+
+const append = ["Milo", "Otis", "Garfield"]
+append.push("Odie")
+
+const prepend = ["Milo", "Otis", "Garfield"]
+prepend.unshift("Odie")
+
+const removeLast = ["Milo", "Otis", "Garfield"]
+removeLast.pop()
+
+const removeFirst = ["Milo", "Otis", "Garfield"]
+removeFirst.shift()
